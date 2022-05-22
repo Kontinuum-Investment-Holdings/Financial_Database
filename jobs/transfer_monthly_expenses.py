08a90f78f27388cc465c47e017ceae20a74ff4ed
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2022-05-22 17:19:30 +1200

    Implemented reserve account transfers.

--- jobs/transfer_monthly_expenses.py
+++ jobs/transfer_monthly_expenses.py
@@ -4,7 +4,7 @@ import global_common
 from finance_database import FinanceDatabase
 from finance_database.exceptions import InsufficientFundsException
 from global_common import Currency
-from wise.models import Transfer, ProfileTypes, CashAccount
+from wise.models import Transfer, ProfileTypes, CashAccount, ReserveAccount, IntraAccountTransfer
 
 import constants
 
@@ -23,6 +23,14 @@ def do(event: None, context: None) -> None:
     savings_transfer: Transfer = Transfer.execute((finance_database.transfers.savings.amount + excess_funds), Currency.NZD, Currency.NZD, finance_database.transfers.savings.account_number, "Savings", ProfileTypes.PERSONAL)
     if finance_database.transfers.needs.amount > 0:
         needs_transfer: Transfer = Transfer.execute(finance_database.transfers.needs.amount, Currency.NZD, Currency.NZD, finance_database.transfers.needs.account_number, "Needs", ProfileTypes.PERSONAL)
+
+    for name, amount in finance_database.reserve.needs_reserve.expenses.items():
+        reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileTypes.PERSONAL, Currency.NZD, f"{name} [Needs Reserve]", True)  # type: ignore
+        intra_account_transfer: IntraAccountTransfer = IntraAccountTransfer.execute(amount, Currency.NZD, reserve_account, ProfileTypes.PERSONAL)  # type: ignore
+
+    for name, amount in finance_database.reserve.wants_reserve.expenses.items():
+        reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileTypes.PERSONAL, Currency.NZD, f"{name} [Wants Reserve]", True)  # type: ignore
+        intra_account_transfer: IntraAccountTransfer = IntraAccountTransfer.execute(amount, Currency.NZD, reserve_account, ProfileTypes.PERSONAL)  # type: ignore
 
 
 if __name__ == "__main__":
