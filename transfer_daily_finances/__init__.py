0da48826832ebd3e9a94a645d7c5e46738af7c73
Kavindu Athaudha <kavindu@k-ih.co.uk>
2022-09-18 02:50:01 +1200

    Changed variables to be compatible with KIH 0.9.30.

--- transfer_daily_finances/__init__.py
+++ transfer_daily_finances/__init__.py
@@ -9,7 +9,7 @@ from kih_api import global_common
 from kih_api.communication import telegram
 from kih_api.finance_database import FinanceDatabase
 from kih_api.global_common import Currency
-from kih_api.wise.models import CashAccount, ProfileTypes, ReserveAccount
+from kih_api.wise.models import CashAccount, ProfileType, ReserveAccount
 
 import constants
 
@@ -21,8 +21,8 @@ def main(timer: functions.TimerRequest) -> None:
 def do() -> None:
     new_zealand_datetime: datetime.datetime = datetime.datetime.now(pytz.timezone("Pacific/Auckland"))
     finance_database: FinanceDatabase = FinanceDatabase(constants.LOCATION_OF_FINANCIAL_DATABASE_FILE, new_zealand_datetime)
-    nzd_account: CashAccount = CashAccount.get_by_profile_type_and_currency(ProfileTypes.PERSONAL, Currency.NZD)
-    monthly_expenses_reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileTypes.PERSONAL, Currency.NZD, constants.MONTHLY_EXPENSES_RESERVE_ACCOUNT_NAME, False)
+    nzd_account: CashAccount = CashAccount.get_by_profile_type_and_currency(ProfileType.Personal, Currency.NZD)
+    monthly_expenses_reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileType.Personal, Currency.NZD, constants.MONTHLY_EXPENSES_RESERVE_ACCOUNT_NAME, False)
 
     monthly_expenses_budget: Decimal = finance_database.transfers.wants.amount
     number_of_days_in_this_month: Decimal = Decimal(str(calendar.monthrange(new_zealand_datetime.date().year, new_zealand_datetime.month)[1]))
@@ -31,7 +31,7 @@ def do() -> None:
     amount_to_transfer: Decimal = monthly_expenses_reserve_account.balance - remaining_monthly_expense_budget
 
     if amount_to_transfer > Decimal("0"):
-        kih_api.wise.models.IntraAccountTransfer.execute(amount_to_transfer, monthly_expenses_reserve_account, nzd_account, ProfileTypes.PERSONAL)
+        kih_api.wise.models.IntraAccountTransfer.execute(amount_to_transfer, monthly_expenses_reserve_account, nzd_account, ProfileType.Personal)
     else:
         telegram.send_message(telegram.constants.telegram_channel_username,
                                             f"<u><b>Monthly Expenses Notification</b></u>"
--- transfer_monthly_finances/__init__.py
+++ transfer_monthly_finances/__init__.py
@@ -6,7 +6,7 @@ from kih_api import global_common
 from kih_api.finance_database import FinanceDatabase
 from kih_api.finance_database.exceptions import InsufficientFundsException
 from kih_api.global_common import Currency
-from kih_api.wise.models import CashAccount, ProfileTypes, Transfer, ReserveAccount, IntraAccountTransfer
+from kih_api.wise.models import CashAccount, ProfileType, Transfer, ReserveAccount, IntraAccountTransfer
 
 import constants
 
@@ -22,7 +22,7 @@ def is_today_last_day_of_month() -> bool:
 @global_common.job("Organising Monthly Expenses")
 def do() -> None:
     financial_database: FinanceDatabase = FinanceDatabase(constants.LOCATION_OF_FINANCIAL_DATABASE_FILE)
-    nzd_account: CashAccount = CashAccount.get_by_profile_type_and_currency(ProfileTypes.PERSONAL, Currency.NZD)
+    nzd_account: CashAccount = CashAccount.get_by_profile_type_and_currency(ProfileType.Personal, Currency.NZD)
     _transfer_to_cash_accounts(nzd_account, financial_database)
     _transfer_to_reserve_accounts(nzd_account, financial_database)
 
@@ -31,21 +31,23 @@ def _transfer_to_cash_accounts(nzd_account: CashAccount, financial_database: Fin
     if excess_funds < 0:
         raise InsufficientFundsException(f"Insufficient funds for monthly expenses\nRequired: NZD {str(financial_database.summary.salary)}\nContains: NZD {str(nzd_account.balance)}\nShort of: NZD {str(-excess_funds)}")
 
-    finance_hub_transfer: Transfer = Transfer.execute(financial_database.transfers.finance_hub.amount, Currency.NZD, Currency.NZD, financial_database.transfers.finance_hub.account_number, "Finance Hub", ProfileTypes.PERSONAL)
-    savings_transfer: Transfer = Transfer.execute((financial_database.transfers.savings.amount + excess_funds), Currency.NZD, Currency.NZD, financial_database.transfers.savings.account_number, "Savings", ProfileTypes.PERSONAL)
+    finance_hub_transfer: Transfer = Transfer.execute(financial_database.transfers.finance_hub.amount, Currency.NZD, Currency.NZD, financial_database.transfers.finance_hub.account_number, "Finance Hub", ProfileType.Personal)
+    savings_transfer: Transfer = Transfer.execute((financial_database.transfers.savings.amount + excess_funds), Currency.NZD, Currency.NZD, financial_database.transfers.savings.account_number, "Savings", ProfileType.Personal)
 
 def _transfer_to_reserve_accounts(nzd_account: CashAccount, financial_database: FinanceDatabase) -> None:
     if financial_database.transfers.needs.amount > 0:
-        needs_transfer: Transfer = Transfer.execute(financial_database.transfers.needs.amount, Currency.NZD, Currency.NZD, financial_database.transfers.needs.account_number, "Needs", ProfileTypes.PERSONAL)
+        needs_transfer: Transfer = Transfer.execute(financial_database.transfers.needs.amount, Currency.NZD, Currency.NZD, financial_database.transfers.needs.account_number, "Needs", ProfileType.Personal)
 
     for name, amount in financial_database.reserve.needs_reserve.expenses.items():
-        reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileTypes.PERSONAL, Currency.NZD, f"{name} [Needs Reserve]", True)
-        intra_account_transfer: IntraAccountTransfer = IntraAccountTransfer.execute(amount, nzd_account, reserve_account, ProfileTypes.PERSONAL)
+        reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileType.Personal, Currency.NZD, f"{name} [Needs Reserve]", True)
+        intra_account_transfer: IntraAccountTransfer = IntraAccountTransfer.execute(amount, nzd_account, reserve_account, ProfileType.Personal)
 
     for name, amount in financial_database.reserve.wants_reserve.expenses.items():
-        reserve_account = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileTypes.PERSONAL, Currency.NZD, f"{name} [Wants Reserve]", True)
-        intra_account_transfer = IntraAccountTransfer.execute(amount, nzd_account, reserve_account, ProfileTypes.PERSONAL)
+        reserve_account = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileType.Personal, Currency.NZD, f"{name} [Wants Reserve]", True)
+        intra_account_transfer = IntraAccountTransfer.execute(amount, nzd_account, reserve_account, ProfileType.Personal)
 
-    nzd_account = CashAccount.get_by_profile_type_and_currency(ProfileTypes.PERSONAL, Currency.NZD)
-    monthly_expenses_reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileTypes.PERSONAL, Currency.NZD, constants.MONTHLY_EXPENSES_RESERVE_ACCOUNT_NAME, True)
-    intra_account_transfer = IntraAccountTransfer.execute(nzd_account.balance, nzd_account, monthly_expenses_reserve_account, ProfileTypes.PERSONAL)
+    nzd_account = CashAccount.get_by_profile_type_and_currency(ProfileType.Personal, Currency.NZD)
+    monthly_expenses_reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(ProfileType.Personal, Currency.NZD, constants.MONTHLY_EXPENSES_RESERVE_ACCOUNT_NAME, True)
+    intra_account_transfer = IntraAccountTransfer.execute(nzd_account.balance, nzd_account, monthly_expenses_reserve_account, ProfileType.Personal)
+
+do()
