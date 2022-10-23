b798e5d25f593495a181730047257313cdecc997
Kavindu Athaudha <kavindu@k-ih.co.uk>
2022-10-23 20:46:40 +1300

    Fixed a bug where Jason's Reserve account is not named properly.

--- organize_funds/__init__.py
+++ organize_funds/__init__.py
@@ -26,9 +26,9 @@ def do() -> None:
     for transaction in transaction_list:
         if isinstance(transaction.entity, str) and transaction.transaction_type == TransactionType.Transfer and transaction.transaction_amount > Decimal("0"):
             if "Smit".lower() in transaction.entity.lower():
-                jason_smit_reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(nzd_account.user_profile.type, nzd_account.currency, "Jason Smit [Reserve]", True)
+                jason_smit_reserve_account: ReserveAccount = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(nzd_account.user_profile.type, nzd_account.currency, "Jason Smit [Rent]", True)
                 IntraAccountTransfer.execute(transaction.transaction_amount, nzd_account, jason_smit_reserve_account, nzd_account.user_profile.type)
-                jason_smit_reserve_account = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(nzd_account.user_profile.type, nzd_account.currency, "Jason Smit [Reserve]", True)
+                jason_smit_reserve_account = ReserveAccount.get_reserve_account_by_profile_type_currency_and_name(nzd_account.user_profile.type, nzd_account.currency, "Jason Smit [Rent]", True)
 
                 telegram.send_message(constants.HOUSEHOLD_FINANCES_CHANNEL_USERNAME,
                                       f"<b><i>Transfer Received from Jason Smit</b></i>"\
