7d9bb09b20c349ba4b044d4b361bb15bc0efc9c4
Kavindu Athaudha <kavindu@k-ih.co.uk>
2022-10-23 21:07:36 +1300

    Assigned the Dev telegram Channel.

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 import os
 
+from kih_api import global_common
+
 LOCATION_OF_FINANCIAL_DATABASE_FILE: str = os.getenv("KIH_API_LOCATION_EXCEL_FILE")
 MONTHLY_EXPENSES_RESERVE_ACCOUNT_NAME: str = "Monthly Expenses"
 TRANSFER_WISE_FINANCE_HUB_API_KEY_ENVIRONMENT_VARIABLE_KEY: str = "TRANSFER_WISE_FINANCE_HUB_API_KEY"
-HOUSEHOLD_FINANCES_CHANNEL_USERNAME = "household_finances_channel"
+HOUSEHOLD_FINANCES_CHANNEL_USERNAME = "household_finances_channel" if global_common.get_environment() == global_common.Environment.PROD else global_common.get_environment_variable("TELEGRAM_CHANNEL_DEBUG_USERNAME")
